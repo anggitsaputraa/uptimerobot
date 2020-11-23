996e13e65eb8c73c2eaefa98f77f25ddc8719bb2
anggitsaputraa <71829157+anggitsaputraa@users.noreply.github.com>
2020-11-23 14:22:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://keraton-agung-indonesia.glitch.me"]
+const urls = ["https://acong-js.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
